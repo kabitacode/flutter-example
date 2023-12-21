088cf488da33a8313bb5b231c40a5ac000ce351b
Muhamad Zulfiqor <siapaa249@gmail.com>
2023-12-21 14:30:18 +0700

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
+import 'package:example/navigation/secondRoute.dart';
 import 'package:flutter/material.dart';
-import 'package:example/models/todos.dart';
-import 'package:example/navigation/detailTodos.dart';
-import 'package:example/navigation/home.dart';
 
 void main() => runApp(
       MaterialApp(
         title: "Example",
         initialRoute: '/',
         routes: {
-          '/': (context) => Home(todo: []),
-          '/detail_todo': (context) => const DetailTodos()
+          // '/': (context) => Home(todo: []),
+          '/': (context) => SecondRoute(),
         },
       ),
     );
--- lib/models/todos.dart
+++
@@ -1,6 +0,0 @@
-class Todos {
-  final String title;
-  final String description;
-
-  const Todos(this.title, this.description);
-}
